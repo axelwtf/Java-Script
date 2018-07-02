7509c0ec4ca2617eaa490f61716e5d8fac7aeb84
www <wolfs.axelw@gmail.com>
2018-07-02 16:29:37 +0200

    fin

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+var une_guerriere ={
+  nom: "Lila",
+  age: 25,
+  taille: 1.60,
+  force: 80,
+  arme: "Hache et un bouclier"
+}
+
+var une_mage ={
+  nom: "Axel",
+  age: 20,
+  taille: 1.76,
+  force: 100,
+  arme: "acune arme"
+}
+
+var une_paysanne ={
+  nom:"Charlotte",
+  age: 25,
+  taille: 1.65,
+  force: 60,
+  arme: "fourche",
+}
+
+var une_princesse ={
+  nom:"Sanae",
+  age: 25,
+  taille: 1.65,
+  force: 50,
+  arme: "tarcha",
+}
